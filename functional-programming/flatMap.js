e0afe176c8523cedf0f440d4c539efc7050d083c
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-06-25 12:02:38 -0400

    Push up new files.

---
+++ functional-programming/flatMap.js
@@ -0,0 +1,22 @@
+const two2Array = [
+  [
+    {
+      stuff1: ['cakes', 'trees']
+    }
+  ],
+  [
+    {
+      stuff2: ['bread', 'cookies']
+    }
+  ],
+  [
+    {
+      stuff3: ['gummies', 'juice']
+    }
+  ],
+  [
+    {
+      stuff4: ['snacks', 'milk']
+    }
+  ]
+];
